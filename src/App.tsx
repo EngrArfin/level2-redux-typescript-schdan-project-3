4c560bcaa0ae2c66325db51b0c2f499f2373937a
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2024-07-08 13:39:53 +0600

    basic update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
-      <h2>welcome</h2>
+      <div>
+        <h1></h1>
+        <Button>Click </Button>
+        <Button>Click </Button>
+        <Button>Click </Button>
+        <Button>Click </Button>
+      </div>
     </>
   );
 }
